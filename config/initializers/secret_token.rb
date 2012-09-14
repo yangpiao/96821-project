37b7c63c3dbf0730975f3682bafac69923753e18
Yang Piao <yp@cmu.edu>
2012-09-13 20:55:18 -0400

    Created new Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project96821::Application.config.secret_token = '9b19fcd0251bccdfeb8f8b3146b0aed7fbe2eb28f2e534aed9d1a1cfd8cb047f254bb1aaf3edeeda754efbd1a1a0afcc6251ded072238db4347ed1523b0f6bf5'
